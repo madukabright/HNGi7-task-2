99f3cdd7beb91ff35032ae2009c244a0b039d0c1
madukabright <66301534+madukabright@users.noreply.github.com>
2020-06-09 02:56:30 +0000

    Create brightsomto-HNG-05447.js

---
+++ brightsomto-HNG-05447.js
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">      
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+    <header>
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">FACILITIES</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ROOMS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                  </li>
+                <li class="nav-item">
+                  <a class="btn btn-bd-download d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="#">BOOK NOW</a>
+                </li>
+              </ul>
+        </nav>
+    </header>
+    
+    <div>
+        <h2>Contact Us</h2>
+        <p>A few hours from Anywhere</p>
+    </div>
+    
+     <div>
+        <h2>Get in Contact with Us</h2>
+        <p>For further information don't hesitate to contact usus via email or at the phone number below</p>
+
+        <figure>
+            <img src="C:\Users\immanuelredd\Desktop\Contacts\Email.jpg" alt="mail">
+            <figcaption>Email</figcaption>
+            <p></p>
+          </figure>
+    
+        <figure>
+            <img src="C:\Users\immanuelredd\Desktop\Contacts\phone.jpg" alt="mail">
+            <figcaption>Phone</figcaption>
+            <p></p>
+        </figure>
+
+        <figure>
+            <img src="C:\Users\immanuelredd\Desktop\Contacts\address.jpg" alt="mail">
+            <figcaption>Address</figcaption>
+            <p></p>
+        </figure>
+        
+     </div>
+    
+
+    <div>
+        <h2>Stay Up to date</h2>
+        <p>Get our emails.Letters from our five stars hotel, news</p>
+        <form action="">
+            <label for="fullname"> Name:
+                <input type="text" name="" id="fullname">
+            </label>
+
+            <label for="mail"> Email:
+                <input type="email" name="" id="mail">
+            </label>
+
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    <footer>
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ROOMS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">FACILITIES</a>
+                </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                  </li>
+              </ul>
+        </nav>
+    </footer>
+</div>   
+</body>
+</html>
